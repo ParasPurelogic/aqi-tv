728b4e26b40dd13f0b7846eaf29495a1746f0a41
Sahil <sahilsworkmail@gmail.com>
2025-02-08 11:29:26 +0530

    ..

--- src/config/misc.ts
+++ src/config/misc.ts
@@ -2,7 +2,7 @@
 export const conventions = {
   cookie: {
     getCookieLife: () => new Date(Date.now() + 150 * 24 * 60 * 60 * 1000),
-    userInfo: "aqi_au_sakdosj98h",
+    userInfo: "aqi_tu_sakdosj98h",
   },
   query: {
   },
